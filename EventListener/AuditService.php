881a76f5ef11c1da1e13c53ec9cfdd2f9a01a10b
tanasecosminromeo@gmail.com <Cosmin Romeo TANASE>
2019-10-25 15:42:13 +0300

    Fix audit to be sf4 compatible

--- EventListener/AuditService.php
+++ EventListener/AuditService.php
@@ -1,14 +1,16 @@
 <?php
 namespace TCR\AuditBundle\EventListener;
 
+use Doctrine\Common\EventSubscriber;
 use Doctrine\Common\Util\ClassUtils;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\PostFlushEventArgs;
+use Doctrine\ORM\Events;
+use Symfony\Component\Security\Core\Security;
 use TCR\AuditBundle\Entity\Audit;
 use TCR\AuditBundle\Entity\Change;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class AuditService
+class AuditService implements EventSubscriber
 {
     private $em;
     private $changes = [];
@@ -20,11 +22,16 @@ class AuditService
      * @var array|false|string
      */
     private $audit_prefix;
+    /**
+     * @var Security
+     */
+    private $security;
 
-    public function __construct()
+    public function __construct(Security $security)
     {
-        $this->audit_classes = getenv('AUDIT_CLASSES') ?? $this->container->getParameter('audit_classes');
-        $this->audit_prefix = getenv('AUDIT_PREFIX') ?? $this->container->getParameter('audit_prefix');
+        $this->security = $security;
+
+        $this->audit_classes = getenv('AUDIT_CLASSES');
 
         if (gettype($this->audit_classes) !== 'array'){
             $this->audit_classes = explode(',', $this->audit_classes);
@@ -34,71 +41,80 @@ class AuditService
     public function preUpdate(LifecycleEventArgs $args)
     {
         $entity = $args->getEntity();
-				$entityName = ClassUtils::getRealClass(get_class($entity));
+        $entityName = ClassUtils::getRealClass(get_class($entity));
+        $cleanName = explode('Entity\\', $entityName);
+        $cleanName = array_pop($cleanName);
 
-				# We only audit the entities we are configured to audit
-				if (!in_array($entityName, $this->audit_classes)) return;
+        # We only audit the entities we are configured to audit
+        if (!in_array($cleanName, $this->audit_classes)) return;
 
-				if (count($args->getEntityChangeSet())<=0) return;
-				$this->em = $args->getEntityManager();
+        if (count($args->getEntityChangeSet())<=0) return;
+        $this->em = $args->getEntityManager();
 
-				$key = $entity->getId()."|".str_replace($this->audit_prefix, '', $entityName);
-				if (!isset($this->changes[$key])){ $this->changes[$key] = []; }
-				foreach ($args->getEntityChangeSet() as $field => $values){
-					if ($values[0]===$values[1]) continue;
-					$this->changes[$key][$field] = $this->formatValue($values[0]);
-				}
+        $key = $entity->getId()."|".$cleanName;
+
+        if (!isset($this->changes[$key])){ $this->changes[$key] = []; }
+        foreach ($args->getEntityChangeSet() as $field => $values){
+            if ($values[0]===$values[1]) continue;
+            $this->changes[$key][$field] = $this->formatValue($values[0]);
+        }
     }
 
     public function postFlush(PostFlushEventArgs $args)
     {
         if (count($this->changes)==0) return;
 
-				$user = $this->getUser();
-				foreach ($this->changes as $key => $changes){
-					list($entityId, $entityName) = explode("|", $key);
+        $user = $this->getUser();
+        foreach ($this->changes as $key => $changes){
+            list($entityId, $entityName) = explode("|", $key);
 
-					$audit = new Audit($entityName, $entityId, $user);
-					foreach ($changes as $field => $value){
-						$change = new Change($audit, $field, $value);
-						$this->em->persist($change);
-						$audit->addChange($change);
-					}
-					$this->em->persist($audit);
-				}
+            $audit = new Audit($entityName, $entityId, $user);
+            foreach ($changes as $field => $value){
+                $change = new Change($audit, $field, $value);
+                $this->em->persist($change);
+                $audit->addChange($change);
+            }
+            $this->em->persist($audit);
+        }
 
-				$this->changes = [];
-				$this->em->flush();
+        $this->changes = [];
+        $this->em->flush();
     }
 
-		# Get the current username
-		private function getUser(){
-			$token = $this->container->get('security.token_storage')->getToken();
-			if (gettype($token)==='object'){
-				$user = $token->getUser();
-				if (gettype($user)==='object'){
-					return $user->getId();
-				}
-			}
+    # Get the current username
+    private function getUser(){
+        $user = $this->security->getUser();
+        if (gettype($user)==='object'){
+            return $user->getId();
+        }
 
-			return 0;
-		}
+        return 0;
+    }
 
-		private function formatValue($value){
-			switch (gettype($value)){
-				case "object":
-					 	$class = get_class($value);
+    private function formatValue($value){
+        switch (gettype($value)){
+            case "object":
+                $class = get_class($value);
 
-						if (strstr($class, '\Entity\\')){
-							return $value->getId();
-						} elseif (strstr($class, 'DateTime')){
-							return date('Y-m-d H:i:s', $value->getTimestamp());
-						} else {
-							return $class;
-						}
-					break;
-				default:
-					return $value;
-			}
-		}
+                if (strstr($class, '\Entity\\')){
+                    return $value->getId();
+                } elseif (strstr($class, 'DateTime')){
+                    return date('Y-m-d H:i:s', $value->getTimestamp());
+                } else {
+                    return $class;
+                }
+                break;
+            default:
+                return $value;
+        }
+    }
+
+
+    public function getSubscribedEvents()
+    {
+        return [
+            Events::preUpdate,
+            Events::postFlush
+        ];
+    }
 }
